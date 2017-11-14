da64d14219608cd14b9eb6909be810204c50c429
Harry <mailsuthan@gmail.com>
2017-11-14 12:08:50 +0000

    Initial commit with clearfiles.bat
    Batch script for clearing Temp & Windows Logs

---
+++ clearFiles.bat
@@ -0,0 +1,23 @@
+@Echo off
+echo This program will clear all event logs and temporary files in Windows 7 (without prompting).
+CHOICE /C YN /M "Are you sure? :" 
+IF ERRORLEVEL 2 goto cancel
+IF ERRORLEVEL 1 GOTO clearFiles
+
+
+
+:clearFiles
+rd /s /q %temp%
+mkdir %temp%
+rd /s /q %WINDIR%\temp\
+mkdir %WINDIR%\temp\
+ipconfig /flushdns
+for /f %x in ('wevtutil el') do wevtutil cl "%x"
+echo "All done!"
+pause
+exit
+
+:cancel
+echo "Nothing is modified!"
+pause
+exit
