0e91b35b06c290358a219463b3d52f29582e6872
Harry <14015057+HarryVasanth@users.noreply.github.com>
2023-01-04 22:04:43 +0000

    Update resetWindowsFirewall.bat

--- network/resetWindowsFirewall.bat
+++ network/resetWindowsFirewall.bat
@@ -1,34 +1,33 @@
 :: Reset Windows Firewall
 :: https://github.com/HarryVasanth/winTools
 
-@echo off
+@echo on
 setlocal ENABLEDELAYEDEXPANSION
 
 :: Check for Admin Rights
-dism >nul 2>&1 || (echo Please run this script as an Administrator. && pause && exit /b 1)
+dism >nul 2>&1 || (echo Please run this script as an Administrator. && pause && goto :eof)
 
 :: User Confirmation
 echo This program will reset your Windows Firewall.
 CHOICE /C YN /M "Are you sure? :" 
-IF ERRORLEVEL 2 goto answerNo
-IF ERRORLEVEL 1 GOTO answerYes
-
+if exist %errorlevel% goto answerNo
+if not exist %errorlevel% GOTO answerYes
 
 :: Reset Firewall Profiles
 :answerYes
 netsh advfirewall set allprofiles state off >nul 2>&1
 netsh advfirewall firewall delete rule all
-netsh advfirewall set allprofiles firewallpolicy blockinbound,blockoutbound >nul 2>&1
+netsh advfirewall set allprofiles firewallpolicy blockinbound,blockoutbound
 netsh advfirewall reset >nul 2>&1
 netsh advfirewall set allprofiles state on >nul 2>&1
 
 :: Done
 echo "All done!"
 pause
-exit /b 0
+goto :eof
 
 :: Cancel Action
 :answerNo
 echo "Nothing is modified!"
 pause
-exit /b 0
+goto :eof
