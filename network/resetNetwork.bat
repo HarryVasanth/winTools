34611f172618f4fe16ff573922602e100d05438e
Harry <14015057+HarryVasanth@users.noreply.github.com>
2023-01-04 22:06:34 +0000

    Update resetNetwork.bat

--- network/resetNetwork.bat
+++ network/resetNetwork.bat
@@ -1,18 +1,18 @@
 :: Reset All Network Settings
 :: https://github.com/HarryVasanth/winTools
 
-@echo off
+@echo on
 setlocal ENABLEDELAYEDEXPANSION
 
 :: Check for Admin Rights
-dism >nul 2>&1 || (echo Please run this script as an Administrator. && pause && exit /b 1)
+echo off
+dism >nul 2>&1 || (echo Please run this script as an Administrator. && pause && goto :eof)
 
 :: User Confirmation
 echo This program will reset your Network Settings.
 CHOICE /C YN /M "Are you sure? :" 
-IF ERRORLEVEL 2 goto answerNo
-IF ERRORLEVEL 1 GOTO answerYes
-
+if exist %errorlevel% goto answerNo
+if not exist %errorlevel% GOTO answerYes
 
 :: Reset Network Settings
 :answerYes
@@ -24,10 +24,10 @@ netsh winsock reset >nul 2>&1
 :: Done
 echo "All done!"
 pause
-exit /b 0
+goto :eof
 
 :: Cancel Action
 :answerNo
 echo "Nothing is modified!"
 pause
-exit /b 0
+goto :eof
